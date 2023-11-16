8e45978401216371fc7d15443f2cd6f38b31406b
Candrandika <candrandikarpl1.kanesa@gmail.com>
2023-11-16 19:07:10 +0700

    add heroes section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import AnimeList from '@/components/AnimeList'
 import SectionHeader from '@/components/SectionHeader'
 import { getAnimeData } from '@/services/api-service'
 import Link from 'next/link'
+import Recomendation from '@/components/Recomendation'
 
 const Page = async () => {
   const top_animes = await getAnimeData("top/anime", "limit=12")
@@ -9,6 +10,7 @@ const Page = async () => {
   return (
     <>
       <section id="popular" className="px-5 mt-5">
+        <Recomendation />
         <SectionHeader title="Most Popular Anime" link_href="/popular" link_title="show all" />
         <AnimeList animes={top_animes} />
       </section>
---
+++ src/components/Recomendation/index.jsx
@@ -0,0 +1,61 @@
+"use client"
+
+import Link from "next/link"
+import Image from "next/image"
+import { getAnimeData } from "@/services/api-service"
+import Carousel from "better-react-carousel"
+
+const Recomendation = async () => {
+    const now_animes = await getAnimeData('seasons/now')
+
+    return (
+        <div>
+            <Carousel
+                cols={5}
+                rows={1}
+                gap={10}
+                loop
+                showDots
+                autoplay={3000}
+                mobileBreakpoint={360}
+                responsiveLayout={[
+                    {
+                        breakpoint: 1024,
+                        cols: 4,
+                        rows: 1,
+                        gap: 10,
+                        loop: true,
+                    },
+                    {
+                        breakpoint: 768,
+                        cols: 3,
+                        rows: 1,
+                        gap: 10,
+                        loop: true,
+                    },
+                    {
+                        breakpoint: 640,
+                        cols: 2,
+                        rows: 1,
+                        gap: 10,
+                        loop: true,
+                    }
+                ]}
+            >
+                {
+                    now_animes?.data?.map((anime, index) => {
+                        return(
+                            <Carousel.Item key={index}>
+                                    <div className="">
+                                        <Image src={anime.images.webp.large_image_url} alt={anime.images.jpg.large_image_url} width={200} height={300} className="fill-image xl:h-96 lg:h-80 md:h-72 sm:h-60 h-52"/>
+                                    </div>
+                            </Carousel.Item>
+                        )
+                    })
+                }
+            </Carousel>
+        </div>
+    )
+}
+
+export default Recomendation
