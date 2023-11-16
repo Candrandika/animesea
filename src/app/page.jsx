47afe50ca453aa6099521c92f26904a899b1feea
Candrandika <candrandikarpl1.kanesa@gmail.com>
2023-11-17 05:05:57 +0700

    add recommended anime section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,23 @@
 import AnimeList from '@/components/AnimeList'
 import SectionHeader from '@/components/SectionHeader'
-import { getAnimeData } from '@/services/api-service'
+import { flattenLimitAnimeData, getAnimeData } from '@/services/api-service'
 import Link from 'next/link'
 import Recomendation from '@/components/Recomendation'
 
 const Page = async () => {
   const top_animes = await getAnimeData("top/anime", "limit=12")
   const now_animes = await getAnimeData('seasons/now')
+  const recommendations = await getAnimeData("recommendations/anime")
+  const recommended_animes = flattenLimitAnimeData(recommendations, 6)
 
   return (
     <>
-      <section id="popular" className="px-5 mt-5">
+      <section id="popular" className="mt-5">
         <Recomendation now_animes={now_animes}/>
         <SectionHeader title="Most Popular Anime" link_href="/popular" link_title="show all" />
         <AnimeList animes={top_animes} />
+        <SectionHeader title="Recommended Animes"/>
+        <AnimeList animes={recommended_animes} />
       </section>
     </>
   )
--- src/services/api-service.js
+++ src/services/api-service.js
@@ -3,3 +3,29 @@ export const getAnimeData =  async(resource = '', query = '') => {
     const data = await response.json()
     return data
 }
+
+export const flattenLimitAnimeDataByIndex = (nested_animes = {}, limit = 0) => {
+    let flatten_data = nested_animes.data?.flatMap((data) => data.entry)
+
+    const min_index = Math.floor(Math.random() * (flatten_data.length-limit) +1)
+
+    if(limit !== 0) flatten_data = flatten_data.splice(min_index, limit)
+    return {data: flatten_data}
+}
+
+export const flattenLimitAnimeData = (nested_animes = {}, limit = 0) => {
+    let flatten_data = nested_animes.data?.flatMap((data) => data.entry)
+
+    let limited_data = [];
+    while(limit !== 0 && limited_data.length < limit) {
+        let random_index = Math.floor(Math.random() * (flatten_data.length-1))
+        
+        if(flatten_data[random_index]) {
+            let already_in = limited_data.some((o) => o.mal_id === flatten_data[random_index].mal_id)
+            if(!already_in) limited_data.push(flatten_data[random_index])
+        }
+    }
+
+    if(limit === 0) limited_data = flatten_data
+    return {data: limited_data}
+}
