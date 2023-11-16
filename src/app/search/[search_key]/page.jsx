5ccbf4d23c359c026b26ad0252f4d24ad536b155
Candrandika <candrandikarpl1.kanesa@gmail.com>
2023-11-17 05:08:28 +0700

    add pagination on search

--- src/app/search/[search_key]/page.jsx
+++ src/app/search/[search_key]/page.jsx
@@ -1,17 +1,31 @@
+"use client"
+
 import SectionHeader from '@/components/SectionHeader'
 import AnimeList from '@/components/AnimeList'
 import { getAnimeData } from '@/services/api-service'
+import { useState, useEffect } from 'react'
+import Pagination from '@/components/utilities/Pagination'
 
 const Page = async ({ params }) => {
     const {search_key} = params 
-    const search_animes = await getAnimeData("anime", `q=${search_key}`)
+    const [page, setPage] = useState(1)
+    const [searchAnimes, setSearchAnimes] = useState([])
+
+    const fetchData = async () => {
+        const search_animes = await getAnimeData("anime", `q=${search_key}&page=${page}`)
+        setSearchAnimes(search_animes)
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [page])
+
     return (
-        <>
-        <section id="search" className="px-5 mt-5">
-            <SectionHeader title={`Result for "${decodeURI(search_key)}"`}/>
-            <AnimeList animes={search_animes} />
+        <section id="search" className="mt-5">
+            <SectionHeader title={`Result for "${decodeURI(search_key)}" page ${page}`}/>
+            <AnimeList animes={searchAnimes} />
+            <Pagination page={page} last_page={searchAnimes?.pagination?.last_visible_page} setPage={setPage}/>
         </section>
-        </>
     )
 }
 
